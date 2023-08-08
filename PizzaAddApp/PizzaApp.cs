a31ddd0d6aad94ac0661eb36aebdd4f0b77559b0
Fedor Baskaev <kingstudy.baskaev@gmail.com>
2023-08-08 14:23:29 +0300

    changes from notebook

---
+++ PizzaAddApp/PizzaApp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PizzaMacker;
+
+namespace PizzaAddApp
+{
+    internal abstract class PizzaApp
+    {
+        List<Dough> doughs;
+        List<Ingredient> souses;
+        List<Ingredient> toppings;
+        public PizzaApp()
+        {
+            doughs = new List<Dough>();
+            souses = new List<Ingredient>();
+            toppings = new List<Ingredient>();
+        }
+        public static void addNewDough(string name, decimal cost)
+        {
+            doughs.Add(new Dough(name, cost));
+        }
+        public static void addNewSouse(string name, decimal cost)
+        {
+            souses.Add(new Ingredient(name, cost));
+        }
+        public static void addNewTopping(string name, decimal cost)
+        {
+            toppings.Add(new Ingredient(name, cost));
+        }
+        public static
+    }
+}
